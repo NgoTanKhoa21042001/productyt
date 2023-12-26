4a0eb913e6a38578c91ee5fc8ee380b50b392f7b
Ngo-Tan-Khoa <khoa.ngo-ojt07@devplus.edu.vn>
2023-12-26 16:20:30 +0700

    fix something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import Image from "next/image";
-import Products from "../components/Products";
-
-const getData = async () => {
-  const res = await fetch("https://jsonserver.reactbd.com/phone");
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-};
 export default async function Home() {
-  const products = await getData();
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Products products={products} />
-    </main>
-  );
+  return <main>s</main>;
 }
